9be1ebce101ed03d1e21e5e2458d656cbc521e98
Hugo Bottois <bottois.hugo@outlook.fr>
2020-08-09 21:31:14 +0200

    refracto

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,17 @@
 class Event < ApplicationRecord
   has_many :users
+
+
+
+
+
+
+  def start_event
+    Date.new(self.start_time.strftime("%y").to_i , self.start_time.strftime("%m").to_i , self.start_time.strftime("%d").to_i)
+  end
+
+  def end_event
+    Date.new(self.ending_at.strftime("%y").to_i , self.ending_at.strftime("%m").to_i , self.ending_at.strftime("%d").to_i)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,34 @@
 class User < ApplicationRecord
   belongs_to :event
   has_many :uptimes
+
+
+
+
+
+
+ # user.uptimes.each do |start|
+ #  startdate_user = Date.new(start.start_time.strftime("%y").to_i , start.start_time.strftime("%m").to_i , start.start_time.strftime("%d").to_i)
+ #  enddate_user = Date.new(start.ending_at.strftime("%y").to_i , start.ending_at.strftime("%m").to_i , start.ending_at.strftime("%d").to_i)
+
+
+  def start
+    starting = ""
+    self.uptimes.each do |start|
+      starting = Date.new(start.start_time.strftime("%y").to_i , start.start_time.strftime("%m").to_i , start.start_time.strftime("%d").to_i)
+    end
+    return starting
+  end
+
+  def end
+    ending = ""
+    self.uptimes.each do |start|
+      ending = Date.new(start.ending_at.strftime("%y").to_i , start.ending_at.strftime("%m").to_i , start.ending_at.strftime("%d").to_i)
+    end
+    return ending
+  end
+
 end
+
+
+
