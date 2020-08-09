09e817f71e197202db749121583430774d512456
Hugo Bottois <bottois.hugo@outlook.fr>
2020-08-09 16:00:46 +0200

    participation row

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,6 @@ class UsersController < ApplicationController
 
     @user = User.new
     @user.event = @event
-
-    # @user = User.new(user_params)
-
-    # @event = Event.find(params[:event_id])
-    # @user.event_id = @event.id
-    # if @user.save
-    #   new_user_uptime_path(@user)
-    # end
   end
 
   def create
