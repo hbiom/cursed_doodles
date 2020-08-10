9d58fa9ebbffcaecc691e01052e587ca36415a12
Hugo Bottois <bottois.hugo@outlook.fr>
2020-08-10 21:36:01 +0200

    refracto

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,4 +14,21 @@ class Event < ApplicationRecord
     Date.new(self.ending_at.strftime("%y").to_i , self.ending_at.strftime("%m").to_i , self.ending_at.strftime("%d").to_i)
   end
 
+  def participants
+
+    participants = Hash.new(0)
+
+    self.users.each do |user|
+      date_user = []
+      (user.start..user.end).each do |date|
+        date_user << date
+      end
+
+      (self.start_event..self.end_event).each do |item|
+        date_user.include?(item) ? participants[item] += 1 : participants[item] += 0
+      end
+    end
+    participants
+  end
+
 end
