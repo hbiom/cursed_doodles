5df904b12a964dde0e7a01cf4336045229ae248f
Hugo Bottois <bottois.hugo@outlook.fr>
2020-08-21 17:56:18 +0200

    add note to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_104341) do
+ActiveRecord::Schema.define(version: 2020_08_21_151119) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_08_09_104341) do
     t.datetime "ending_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "note"
   end
 
   create_table "uptimes", force: :cascade do |t|
